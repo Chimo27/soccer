8c56b3adc9264542cc8258c7ca47f2dfb8572eac
chimo <kachcharmohamed01@gmail.com>
2024-12-29 16:38:37 +0100

    using gridsearchcv to get best params for our knn model

--- Pnd.py
+++ Pnd.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import cross_val_score
 import numpy as np
+from sklearn.model_selection import GridSearchCV
 
 
 iris = load_iris()
@@ -16,8 +17,17 @@ y = iris.target
 
 model = KNeighborsClassifier(n_neighbors=6)
 x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=5)
-# k= np.ndarray()
-print(cross_val_score(model , x_train,y_train,scoring="accuracy",cv=5))
+k= np.linspace(1,50,50,dtype=int)
+
+params = param_grid = {
+    'n_neighbors': k,  # Number of neighbors to consider
+    'weights': ['uniform', 'distance'],  # Weight function used in prediction
+    'metric': ['euclidean', 'manhattan', 'minkowski']  # Distance metric
+}
+grid = GridSearchCV(KNeighborsClassifier(),param_grid=params,cv=5,return_train_score=True)
+grid.fit(x_train,y_train)
+print(grid.best_params_)
+# cross_val_score(model , x_train,y_train,scoring="accuracy",cv=5) 
 # model.fit(x_train,y_train)
 # print(model.score(x_test,y_test))
 # print(x)
